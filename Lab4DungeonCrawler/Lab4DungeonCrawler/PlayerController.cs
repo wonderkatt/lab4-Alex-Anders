0800166681ac71efbf85c698c1b248c92a3d76cc
Alexander Bertilsson <alexander.bertilsson@outlook.com>
2019-09-19 16:01:08 +0200

    restore PLayerController

---
+++ Lab4DungeonCrawler/Lab4DungeonCrawler/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4DungeonCrawler
+{
+    public class PlayerController
+    {
+        
+        public readonly TileMap map;
+        private readonly Player player;
+
+        public PlayerController(TileMap map, Player player)
+        {
+            this.map = map;
+            this.player = player;
+        }
+
+        public void MovePlayer()
+        {
+            var input = Console.ReadKey();
+
+            switch(input.KeyChar)
+            {
+                case 'w':
+                    Move(0, -1);
+                    break;
+                case 's':
+                    Move(0, 1);
+                    break;
+                case 'a':
+                    Move(-1, 0);
+                    break;
+                case 'd':
+                    Move(1, 0);
+                    break;
+            }
+        }
+         
+        private void Move(int row, int column)
+        {
+            map.map[player.currentPosition.column, player.currentPosition.row] = new FloorTile();
+            player.currentPosition = new Point(player.currentPosition.row + row, player.currentPosition.column + column);
+            map.map[player.currentPosition.column, player.currentPosition.row] = new PlayerTile();
+        }
+        
+    }
+}
